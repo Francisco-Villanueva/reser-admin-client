8aab33dc3ab2fa79702d6e3e3521a3920bb48763
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-12-19 17:13:22 -0300

    feat (appointment): new implementation of appointLis

--- src/app/home/admin/turnos/page.js
+++ src/app/home/admin/turnos/page.js
@@ -1,7 +1,39 @@
 "use client";
+import Button from "@/commons/Button";
+import ChildrenLayout from "@/commons/ChildrenLayout";
 import AppointmentsList from "@/components/AppointmentsList";
-import React from "react";
+import { useStore } from "@/context/AdminContext";
+import { AuthServices } from "@/services";
+import React, { useEffect } from "react";
 
 export default function TunosListPage() {
-  return <AppointmentsList />;
+  const { barbers, selectedBarber, setSelectedBarber, currentUser } =
+    useStore();
+
+  useEffect(() => {
+    if (!selectedBarber.id) setSelectedBarber(currentUser);
+  }, []);
+  const handleSelectBarber = (barberId) => {
+    AuthServices.getBarber(barberId).then((res) => {
+      setSelectedBarber(res.data);
+    });
+  };
+  return (
+    <ChildrenLayout>
+      <section className="flex gap-4 p-2">
+        {barbers.map((barber) => (
+          <Button
+            variant={`${
+              !(barber.id === selectedBarber.id) ? "primary" : "secondary"
+            }`}
+            className="  p-2 rounded-md "
+            onClick={() => handleSelectBarber(barber.id)}
+          >
+            {barber.name}
+          </Button>
+        ))}
+      </section>
+      <AppointmentsList barber={selectedBarber} />
+    </ChildrenLayout>
+  );
 }
--- src/components/AppointmentsList.jsx
+++ src/components/AppointmentsList.jsx
@@ -4,22 +4,28 @@ import TableTeamRow from "./TableTeamRow";
 import Loader from "./Loader";
 import ChildrenLayout from "@/commons/ChildrenLayout";
 import TitleView from "@/commons/TitleView";
+import { getFutureAppointments } from "@/utils/futureAppointments";
 
-export default function AppointmentsList() {
+export default function AppointmentsList({ barber }) {
   const {
     currentUser: { appointments },
   } = useStore();
+
+  const appointmentsToShow =
+    barber && barber.id ? barber.appointments : appointments;
+  const futureAppointments = getFutureAppointments(appointmentsToShow);
   return (
-    <ChildrenLayout className="flex flex-col gap-4  max-sm:gap-2 overflow-auto">
-      <TitleView>Próximos Turnos</TitleView>
+    <ChildrenLayout className="flex flex-col gap-4   max-sm:gap-2 overflow-auto">
+      <TitleView>Próximos Turnos: </TitleView>
       <hr />
+      {futureAppointments.length === 0 && (
+        <span className="p-2 w-full bg-error text-white rounded-lg">
+          No hay turnos
+        </span>
+      )}
       {appointments ? (
-        appointments.length ? (
-          <TableTeamRow barber={appointments} />
-        ) : (
-          <span className="p-2 w-full bg-error text-white rounded-lg">
-            No hay turnos
-          </span>
+        futureAppointments.length > 0 && (
+          <TableTeamRow appointments={futureAppointments} />
         )
       ) : (
         <Loader />
