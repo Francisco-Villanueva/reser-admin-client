36b63c1480b1c4a31e8aff54a698197bc60b0c27
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-12-20 17:26:37 -0300

    added api key

--- src/services/appointment.services.js
+++ src/services/appointment.services.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_KEY = process.env.NEXT_API_KEY;
 
 export class AppointmentServices {
   static async delete(id) {
-    return await axios.delete(`${API_URL}/appointment/${id}`);
+    return await axios.delete(
+      `${API_URL}/appointment/${id}/?API_KEY=${API_KEY}`
+    );
   }
 }
--- src/services/workhours.services.js
+++ src/services/workhours.services.js
@@ -1,26 +1,37 @@
 import axios from "axios";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_KEY = process.env.NEXT_API_KEY;
 
 export class ApiServices {
   static async getAllBarbers() {
-    return await axios.get(`${API_URL}/barbers`);
+    return await axios.get(`${API_URL}/barbers/?API_KEY=${API_KEY}`);
   }
   static async getHoursByDay(barberId, dayNumber) {
-    return await axios.get(`${API_URL}/workhours/${barberId}/${dayNumber}`);
+    return await axios.get(
+      `${API_URL}/workhours/${barberId}/${dayNumber}/?API_KEY=${API_KEY}`
+    );
   }
   static async createBarber(data) {
-    return await axios.post(`${API_URL}/barbers`, data);
+    return await axios.post(`${API_URL}/barbers/?API_KEY=${API_KEY}`, data);
   }
   static async updateBarberData(barberId, data) {
-    return await axios.put(`${API_URL}/barbers/${barberId}`, { ...data });
+    return await axios.put(
+      `${API_URL}/barbers/${barberId}/?API_KEY=${API_KEY}`,
+      { ...data }
+    );
   }
   static async deleteBarber(barberId) {
-    return await axios.delete(`${API_URL}/barbers/${barberId}`);
+    return await axios.delete(
+      `${API_URL}/barbers/${barberId}/?API_KEY=${API_KEY}`
+    );
   }
   static async updateBarberWorkHours(barberId, dayNumber, data) {
-    return await axios.put(`${API_URL}/workhours/${barberId}/${dayNumber}`, {
-      hours: data,
-    });
+    return await axios.put(
+      `${API_URL}/workhours/${barberId}/${dayNumber}/?API_KEY=${API_KEY}`,
+      {
+        hours: data,
+      }
+    );
   }
 }
