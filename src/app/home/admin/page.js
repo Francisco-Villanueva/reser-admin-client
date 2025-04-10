efb382a760a8f28fa77eeb52a7dbd23765001643
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2025-04-10 15:56:37 -0300

    Fix DASHBOARD UI

--- src/app/home/admin/page.js
+++ src/app/home/admin/page.js
@@ -17,33 +17,31 @@ export default function AdminPage() {
   if(fetchingBarbers || barbers.length === 0) return <LoaderWrapper/> 
   return (
     <ChildrenLayout >
-      <>
-      <section className=" flex  flex-col h-full justify-between gap-4   overflow-auto   ">
-        <div className=''>
-          <GridTeam  />
-        </div>
-        
-        <div className='flex flex-col gap-2 flex-grow brder h-[70%] '>
-        <div className='flex justify-center items-center gap-4 font-semibold text-2xl max-md:text-lg'>
-          <h2>Notificaciones</h2>
-          <BellIcon/>
-        </div>
-        <div className='flex items-start gap-8 h-full px-4 max-lg:flex-col  max-lg:items-center'>
-            <PickCalendar/>
-            <AppointmentList/>
+      <div className='container h-full'>
+        <section className=" flex  flex-col h-full   gap-4     ">
+          <div className=''>
+            <GridTeam  />
           </div>
-        </div>
-      </section>
+          
+          <div className='flex flex-col gap-2 flex-grow py-2 h-[80%]  '>
+              <div className='flex items-start gap-8 h-full px-4 max-lg:flex-col   max-lg:items-center'>
+                  <PickCalendar/>
+                  <div className='h-full w-full max-h-full overflow-auto'>
+                   <AppointmentList/>
+                  </div>
+              </div>
+          </div>
+        </section>
 
-      <div className="h-10 w-10 absolute bottom-0 right-0   rounded-full ">
-        <Button
-          className={' rounded-full h-full w-full p-0 '}
-          onClick={()=>setAside('newBarber')}
-        >
-          <AddIcon />
-        </Button>
+        <div className="h-10 w-10 absolute bottom-0 right-0   rounded-full ">
+          <Button
+            className={' rounded-full h-full w-full p-0 '}
+            onClick={()=>setAside('newBarber')}
+          >
+            <AddIcon />
+          </Button>
+        </div>
       </div>
-      </>
      
     </ChildrenLayout>
   )
--- src/components/Tables/AppointmentList.tsx
+++ src/components/Tables/AppointmentList.tsx
@@ -5,34 +5,32 @@ import React, { useState } from 'react'
 import { Badge } from '../ui/badge'
 import useDate from '@/hooks/useDate'
 import Image from 'next/image'
-import { Button } from '../ui/button'
 import { Contact } from '@/commons/AppointmentCard'
 
 export default function AppointmentList() {
   const { barbers, appointmentsByDate, dateOfFilter } = useStore()
   const { formatToYMD } = useDate()
 
-  const [listLimit, setListLimit] = useState(4)
+  const [listLimit, setListLimit] = useState(15)
   const lastAppoitnments = appointmentsByDate.slice(
     appointmentsByDate.length - listLimit,
   )
   return (
     <div className="flex flex-col gap-2  max-h-[90%] h-[90%] md:overflow-auto    w-full    ">
+      <p className="text-gray-500 font-medium">
+        {' '}
+        Ultimos {lastAppoitnments.length} turnos agendados{' '}
+      </p>
       {lastAppoitnments.length ? (
         lastAppoitnments.map((appointment: Appointment) => (
           <section
-            className=" flex flex-col gap-4  border border-accent  p-4 rounded-lg"
+            className=" flex flex-col gap-2  border border-border  p-2 rounded-lg"
             key={appointment.id}
           >
-            <div className=" flex max-md:flex-col gap-4 justify-between">
+            <div className=" flex max-md:flex-col gap- justify-between ">
               <div className=" flex-grow">
-                <section className="flex flex-col gap-1 ">
-                  <p className="font-semibold text-lg">{appointment.name}</p>
-                  <hr className="border border-accent w-40" />
-                  <p className="font-normal text-sm">{appointment.phone}</p>
-                  <p className="font-normal text-sm">{appointment.email}</p>
-                </section>
-                <p className="my-2 text-sm text-primary/80 font-light">
+                <p className="font-semibold text-lg">{appointment.name}</p>
+                <p className="text-sm text-gray-600 ">
                   Nuevo turno agenadado 🎉 con{' '}
                   <strong className="font-bold text-primary/90">
                     {
@@ -44,18 +42,16 @@ export default function AppointmentList() {
                   </strong>
                 </p>
               </div>
-              <section className="flex flex-col items-end ">
+              <section className="flex flex-col gap-2 items-end ">
                 <Badge variant="secondary">
                   <div className="flex items-center gap-2">
                     <p className=" ">{formatToYMD(appointment.date)}</p>
                     <p className="">{appointment.time}</p>
                   </div>
                 </Badge>
+                <Contact phoneNumber={appointment.phone} />
               </section>
             </div>
-            <section className=" py-2 flex gap-2 justify-end border-t border-accent">
-              <Contact phoneNumber={appointment.phone} />
-            </section>
           </section>
         ))
       ) : (
